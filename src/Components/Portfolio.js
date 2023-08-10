a925332298b53bcbc15339dad1472235500997f5
Tasnin <tasnintithi@gmail.com>
2023-08-10 09:46:40 +0600

    changes commit

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import the AOS styles
 import img1 from '../assets/portfolio/img1.png';
 import img2 from '../assets/portfolio/img2.png';
 import img3 from '../assets/portfolio/img3.png';
 import img4 from '../assets/portfolio/img4.png';
 
 const Portfolio = () => {
+    AOS.init();
     const portfolios = [
         {
             id: 1,
@@ -38,7 +41,8 @@ const Portfolio = () => {
                     <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Portfolio</p>
                     <p className='py-6'>Check out some of my works right here </p>
                 </div>
-                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 px-12 sm:px-8 ml-[-15px]'>
+                <div data-aos="fade-up"
+                    data-aos-duration="3000" className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 px-12 sm:px-8 ml-[-15px]'>
                     {
                         portfolios.map(({ id, src, liveSiteCode, code }) => (
                             <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
@@ -48,7 +52,7 @@ const Portfolio = () => {
                                     <a href={liveSiteCode} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'
                                         target="_blank"
                                         rel='noreferrer'>Demo</a>
-                                    <a href={code} className='w-1/2 px-6 py-3 mr-[-90px] m-4 duration-200 hover:scale-105'
+                                    <a href={code} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 sm:mr-[-90px] md:mr-[-90px] lg:mr-[-90px] xl:mr-[-90px]'
                                         target="_blank"
                                         rel='noreferrer'>Code</a>
                                 </div>
